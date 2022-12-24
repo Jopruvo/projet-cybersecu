3e893a6c5876ec90531d2c9fbf233c72e54075c5
Boughanmi Rami <rami.boughanmi@etu.u-pec.fr>
2022-12-24 15:12:09 +0100

    Rami version of proxy - only http handled

---
+++ main.py
@@ -0,0 +1,98 @@
+from _thread import start_new_thread
+import socket, sys
+import threading
+
+listening_port = 8080
+max_conn = 5
+buffer_size = 4096
+
+
+def proxy_server(webserver, port, conn, client_data, addr):
+    # we create a new socket that will send the request for the client
+    global server_socket
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.connect((webserver, port))
+        server_socket.send(client_data.encode('utf-8'))
+
+        while True:
+            response = server_socket.recv(buffer_size)
+
+            if (len(response) > 0):
+                conn.send(response)
+                print(f'[*] Request done ! {addr[0]}')
+                print('Response : ',response)
+            else:
+                conn.send(response)
+                break
+        server_socket.close()
+        conn.close()
+    except socket.error:
+        server_socket.close()
+        conn.close()
+        sys.exit(1)
+
+
+def conn_string(conn, client_data: bytes, addr):
+    """
+    This function take the client request and find the webserver dns and the port of the request.
+    :param conn:
+    :param client_data:
+    :param addr:
+    :return:
+    """
+    client_data = client_data.decode()
+    # print("CLIENT DATA: ", client_data)
+    try:
+        first_line = client_data.split('\n')[0]
+        print(f'First line : {first_line}')
+        url = first_line.split(' ')[1]
+        http_pos = url.find("://")  # Find the pos of ://
+        if (http_pos == -1):
+            temp = url
+        else:
+            temp = url[(http_pos + 3):]  # Get the rest of the url
+        port_pos = temp.find(":")  # get the index of the port
+        webserver_pos = temp.find("/")  # get the index of the end of the webserver
+
+        if webserver_pos == -1:
+            webserver_pos = len(temp)
+        webserver = ""
+        port = -1
+        if (port_pos == -1 or webserver_pos < port_pos):  # default port 80
+            port = 80
+            webserver = temp[:webserver_pos]
+        else:
+            port = int((temp[port_pos + 1:])[:webserver_pos - port_pos - 1])
+            webserver = temp[:port_pos]
+        print(f'Webserver : {webserver} \nPort : {port}')
+        proxy_server(webserver, port, conn, client_data, addr)
+    except Exception as e:
+        print("ERRR", e)
+
+
+def start():
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # init socket
+        print("[*] Initializing Sockets")
+        client_socket.bind(('localhost', listening_port))
+        print("[*] Socket bind Successfully")
+        client_socket.listen(max_conn)
+        print(f"[*] Server starting to listen on : {listening_port}")
+    except Exception as e:
+        print(f"[*] Error {e} \n[*] Unable to initialize Socket")
+        sys.exit(2)
+
+    while True:
+        try:
+            conn, addr = client_socket.accept()
+            client_data = conn.recv(buffer_size)
+            # threading.Thread(target=conn_string, args=(conn, client_data, addr))
+            start_new_thread(conn_string, (conn, client_data, addr))
+        except KeyboardInterrupt:
+            client_socket.close()
+            print(f"[*] KeyboardInterrupt :-> Proxy server Shutting Down")
+            sys.exit()
+
+
+start()
