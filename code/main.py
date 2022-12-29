4d9834b81f1b56be46a90173fdbce9357f9f3db0
Zendorkria <44678661+xerox123dshdhwx@users.noreply.github.com>
2022-12-29 16:47:27 +0100

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 ##--------------------------------------------------------------------------------------------##
 ##                                 SUJET 1 - PROJET CYBERSECURITE                             ##
-##  authors : PRUVOST Jordan, TAKAHASHI Vincent, OUKZIZ Salma, GONAY Arthur & BOUGHAMNI Rami  ##
+##  authors : GONAY Arthur & BOUGHAMNI Rami , PRUVOST Jordan, TAKAHASHI Vincent, OUKZIZ Salma ##
 ##                                       date : 27/10/22                                      ##
 ##--------------------------------------------------------------------------------------------##
 
